929af1015b7db302809c286b1f02bcb306b47701
markandey210 <markandeymishra.2013@gmail.com>
2021-06-14 13:09:18 +0530

    DesignPattern file added
    DesignPattern file added

---
+++ DesignPattern.js
@@ -0,0 +1,32 @@
+import React,{ Component } from 'react';
+import { View,Text,StyleSheet} from 'react-native'
+class Demo extends Component {
+      constructor(props) {
+          super(props)
+          this.state = {
+              //Define state
+          }
+      }
+
+    componentWillMount () {
+    }
+    componentDidMount () {
+    }
+    componentWillReceiveProps (nextProps) {
+    }
+    shouldComponentUpdate (nextProps,nextState) {
+    }
+    componentWillUpdate (nextProps,nextState) {
+    }
+    componentDidUpdate (prevProps,prevState) {
+    }
+    componentWillUnmount () {
+    }
+
+    render () {
+        return (
+             <View></view>
+            )
+    }
+
+}
